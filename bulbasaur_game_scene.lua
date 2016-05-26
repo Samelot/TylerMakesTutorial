72321b97e1e14ed011969ef104bdf6bbc7fc7ce5
Sam Englander <sizeoverload@gmail.com>
2016-05-26 09:41:45 -0400

    myRectangle and moveBlock

--- bulbasaur_game_scene.lua
+++ bulbasaur_game_scene.lua
@@ -1,27 +1,37 @@
 local composer = require( "composer" )
 local scene = composer.newScene()
 
--- local game
+local myRectangle = nil
 
 local function onEnterFrame()
-    game:update()
+    --myRectangle.x = myRectangle.x + 10
 end
 
 function scene:create( event )
     local group = self.view
-end
 
-function scene:show( event )
-    local group = self.view
-
-    local myRectangle = display.newRect( 0, 0, 150, 50 )
+    myRectangle = display.newRect( 0, 0, 150, 50 )
     myRectangle.strokeWidth = 3
     myRectangle:setFillColor( 1 )
     myRectangle:setStrokeColor( 1, 0, 0 )
     myRectangle.anchorX = 0
     myRectangle.anchorY = 0
 
+    group:insert(myRectangle)
+
+    local function moveBlock( event )
+        if (event.phase == "began") then
+            myRectangle.x = myRectangle.x + 10
+        end
+        return true
+    end
+
     Runtime:addEventListener( "enterFrame", onEnterFrame )
+    group:addEventListener("touch", moveBlock)
+end
+
+function scene:show( event )
+    local group = self.view
 end
 
 function scene:exit( event )
